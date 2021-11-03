49665412074ab254b4c804b3c5e1717b82da631c
duongd08 <duongdaniel08@gmail.com>
2021-11-02 22:06:53 -0700

    merged the enryption part and fixed rendering issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'dart:html';
+
+
 import 'dart:io' as io;
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -24,40 +25,40 @@ class EncryptingScreen extends StatefulWidget {
 class _EncryptingScreenState extends State<EncryptingScreen> {
 
 
-    late File theImage = File("/images/wallSteerts.png");
+  late io.File theImage = io.File("/images/wall_street.png");
 
-   //use the text editing controller to store the user text input to store it in the images
-   late TextEditingController HiddenMessageController;
-   late TextEditingController PrivateKeyController;
+  //use the text editing controller to store the user text input to store it in the images
+  late TextEditingController HiddenMessageController;
+  late TextEditingController PrivateKeyController;
 
-   late String encryptedFileDir = "";
-   late String HiddenMessage = "";
-   late String PrivateKey = "";
-   bool IsLoaded = false;
+  late String encryptedFileDir = "";
+  late String HiddenMessage = "";
+  late String PrivateKey = "";
+  bool IsLoaded = false;
 
-   void initState(){
+  void initState(){
 
-     HiddenMessageController = new TextEditingController();
-     PrivateKeyController = new TextEditingController();
-     print(HiddenMessageController.text);
+    HiddenMessageController = new TextEditingController();
+    PrivateKeyController = new TextEditingController();
+    print(HiddenMessageController.text);
 
-     super.initState();
-   }
+    super.initState();
+  }
 
-   //when every image is updated or no longer in use, delete the previous data off the text editing controller
-   void dispose() {
-     HiddenMessageController.dispose();
-     PrivateKeyController.dispose();
-     super.dispose();
-   }
+  //when every image is updated or no longer in use, delete the previous data off the text editing controller
+  void dispose() {
+    HiddenMessageController.dispose();
+    PrivateKeyController.dispose();
+    super.dispose();
+  }
 
 
-   void OpenAlbum(BuildContext context) async {
-     var photo = await ImagePicker.pickImage(source: ImageSource.gallery);
-     setState(() {
-         theImage = photo;
-     });
-     Navigator.of(context).pop();
+  void OpenAlbum(BuildContext context) async {
+    var photo = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      theImage = photo;
+    });
+    Navigator.of(context).pop();
   }
 
   void encryptText(BuildContext context, String? password, String? hiddenMessage) async {
@@ -94,40 +95,40 @@ class _EncryptingScreenState extends State<EncryptingScreen> {
 
   Future<void> ShowOptionDialog(BuildContext context) {
     return showDialog(context: context, builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("Select from either option: "),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                  GestureDetector(
-                    child: Text("Album"),
-                    onTap: () {
-                      OpenAlbum(context);
-                    },
-                  ),
+      return AlertDialog(
+        title: Text("Select from either option: "),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              GestureDetector(
+                child: Text("Album"),
+                onTap: () {
+                  OpenAlbum(context);
+                },
+              ),
 
-                  Padding(padding: EdgeInsets.all(10.0)),
-                  /*GestureDetector(
+              Padding(padding: EdgeInsets.all(10.0)),
+              /*GestureDetector(
                     child: Text("Camera"),
                     onTap: () {
                       OpenCamera(context);
                   },
                 ),*/
-              ],
-            ),
+            ],
           ),
-        );
+        ),
+      );
     });
   }
 
-   Widget UpdateImageView(){
+  Widget UpdateImageView(){
     print(theImage);
 
     if(theImage == null) {
       return Text("Image not selected");
     }
 
-    return Image.file(theImage, width: 200, height: 200);
+    return Image.file(theImage, width: 100, height: 100);
 
   }
 
@@ -136,273 +137,48 @@ class _EncryptingScreenState extends State<EncryptingScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Encrypt'n"),
-       ),
+      ),
       body: Container(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-                  UpdateImageView(),
-                  RaisedButton(onPressed: () {
-                  ShowOptionDialog(context);
-              },
-                child: Text("Upload Image"),
-              ),
-
-              //Add the user input for the encrypted message and password to be stored as bits
-
-              SizedBox(height: 20),
-              TextFormField(
-                  controller: HiddenMessageController,
-                  decoration: InputDecoration(
-                  isDense: true,
-                  border: OutlineInputBorder(),
-                  hintText: "Enter the message you want to hide.",
-                ),
-              ),
-
-              SizedBox(height: 20),
-              TextFormField(
-                  controller: PrivateKeyController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                  isDense: true,
-                  border: OutlineInputBorder(),
-                  hintText: "Enter a password.",
-              ),
-              ),
-              FloatingActionButton(
-                  onPressed: () {
-                    encryptText(context, Text(HiddenMessageController.text).data, Text(PrivateKeyController.text).data);
-                  }
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-//void main() => runApp(MyApp());
-
-/*class MyApp extends StatefulWidget {
-  MyApp() : super();
-
-  final String stenAppTitle = "LockItt";
-
-  @override
-  _PickImageDemoState createState() => _PickImageDemoState();
-}
-
-class _PickImageDemoState extends State<MyApp> {
-  late Future<File> imageFile;
-
-  pickImageFromGallery(ImageSource source) {
-    setState(() {
-      imageFile = ImagePicker.pickImage(source: source);
-    });
-  }
-
-  Widget showImage() {
-    return FutureBuilder<File>(
-      future: imageFile,
-      builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
-        if (snapshot.connectionState == ConnectionState.done &&
-            snapshot.data != null) {
-          return Image.file(
-            snapshot.data,
-            width: 300,
-            height: 300,
-          );
-        } else if (snapshot.error != null) {
-          return const Text(
-            'Error Picking Image',
-            textAlign: TextAlign.center,
-          );
-        } else {
-          return const Text(
-            'No Image Selected',
-            textAlign: TextAlign.center,
-          );
-        }
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            showImage(),
-            RaisedButton(
-              child: Text("Select Image from Gallery"),
-              onPressed: () {
-                pickImageFromGallery(ImageSource.gallery);
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-} */
-
-/*class MyApp extends StatefulWidget {
-
-  @override
-  _LockIttState createState() => _LockIttState();
-}
-
-class _LockIttState extends State<MyApp> {
-  late File theImage = File(theImage.path);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("LockItt"),
-        ),
-        body: Container(
-            child: theImage == null
-                ? Container(
-              alignment: Alignment.center,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  RaisedButton(
-                    color: Colors.greenAccent,
-                    onPressed: () {
-                      RetrieveFromGallery();
-                    },
-                    child: Text("Select an Image from gallery"),
-                  ),
-                  Container(
-                    height: 40.0,
-                  ),
-                  RaisedButton(
-                    color: Colors.lightGreenAccent,
-                    onPressed: () {
-                      RetrieveFromCamera();
-                    },
-                    child: Text("Select image from camera"),
-                  )
-                ],
-              ),
-            ): Container(
-              child: Image.file(
-                theImage,
-                fit: BoxFit.cover,
-              ),
-            )));
-
-  }
-
-  /// obtain from gallery
-  RetrieveFromGallery() async {
-    PickedFile selectedImage = await ImagePicker().getImage(
-      source: ImageSource.gallery,
-      maxWidth: 800,
-      maxHeight: 800,
-    );
-    if (selectedImage != null) {
-      setState(() {
-        theImage = File(selectedImage.path);
-      });
-    }
-  }
-
-  /// obtain from Camera
-  RetrieveFromCamera() async {
-    PickedFile selectedImage = await ImagePicker().getImage(
-      source: ImageSource.camera,
-      maxWidth: 800,
-      maxHeight: 800,
-    );
-    if (selectedImage != null) {
-      setState(() {
-        theImage = File(selectedImage.path);
-      });
-    }
-  }
-}
-*/
-
-
-/*class UserTextInput extends StatelessWidget {
-  const UserTextInput({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter the message you want to hide.',
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 3),
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter the password you want to use',
-            ),
-            obscureText: true,
-          ),
-        ),
-      ],
-    );
-  }
-}*/
-
           child: SingleChildScrollView(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
-                    UpdateImageView(),
-                    RaisedButton(onPressed: () {
-                    ShowOptionDialog(context);
+                UpdateImageView(),
+                RaisedButton(onPressed: () {
+                  ShowOptionDialog(context);
                 },
                   child: Text("Upload Image"),
                 ),
 
                 //Add the user input for the encrypted message and password to be stored as bits
 
-                SizedBox(height: 5),
+                SizedBox(height: 20),
                 TextFormField(
-                    controller: HiddenMessageController,
-                    decoration: InputDecoration(
+                  controller: HiddenMessageController,
+                  decoration: InputDecoration(
                     isDense: true,
                     border: OutlineInputBorder(),
                     hintText: "Enter the message you want to hide.",
-
                   ),
                 ),
 
-                SizedBox(height: 5),
+                SizedBox(height: 20),
                 TextFormField(
-                    controller: PrivateKeyController,
-                    obscureText: true,
-                    decoration: InputDecoration(
+                  controller: PrivateKeyController,
+                  obscureText: true,
+                  decoration: InputDecoration(
                     isDense: true,
                     border: OutlineInputBorder(),
                     hintText: "Enter a password.",
+                  ),
                 ),
-                ),
+                FloatingActionButton(
+                    onPressed: () {
+                      encryptText(context, Text(HiddenMessageController.text).data, Text(PrivateKeyController.text).data);
+                    }
+                )
               ],
-
             ),
           ),
         ),
@@ -410,4 +186,3 @@ class _LockIttState extends State<MyApp> {
     );
   }
 }
-
