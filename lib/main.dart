c5d87ef8fce38f126ba50868cac915bdb4c6f317
toasterrmaker <61596703+toasterrmaker@users.noreply.github.com>
2021-11-11 02:59:29 -0800

    added some encryption

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'dart:typed_data';
@@ -6,6 +7,8 @@ import 'package:flutter/services.dart';
 import 'package:image/image.dart' as im;
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
+//import 'package:simple_permissions/simple_permissions.dart';
+
 
 
 void main() {
@@ -25,7 +28,7 @@ class EncryptingScreen extends StatefulWidget {
 
 class _EncryptingScreenState extends State<EncryptingScreen> {
 
-  late File theImage = File("/images/kirby.jpg");
+  late File theImage = File("images/dummy.jpeg");
   late Uint8List decodedImageBytes;
 
   //use the text editing controller to store the user text input to store it in the images
@@ -69,27 +72,25 @@ class _EncryptingScreenState extends State<EncryptingScreen> {
     final path = await _localPath;
     try {
       File file = File('$path/encryption.txt.aes');
-      print('Hidden msg: $password\n');
-      print('Password msg: $hiddenMessage\n');
+      print('Hidden msg: $hiddenMessage\n');
+      print('Password msg: $password\n');
       encryptedFileDir = crypt.encryptTextToFileSync(hiddenMessage, file.path, utf16: false);
       var decryptedString = crypt.decryptTextFromFileSync(file.path);
       print('Contents:' + decryptedString);
       print('Encrypted file: ' + file.path + '\n');
 
-
-      //For testing purposes
-      ByteData byteData = convertFileToByteData(file);
-      print("Converted file to byte data");
-      print(convertByteDataToString(byteData));
-      print("Converted bytedata to uint8list");
-
-      print("Image bytes: ");
-      print(decodeImageData());
-
+      //Steganography portion
+      final newImgAsByteList = putFileBytesIntoImgBytes(file);
+      //Save bytelist to local dir
+      saveBytesAsFile(newImgAsByteList);
+      //Save to gallery
+      //saveImage();
     } catch(e) {
       print(e.toString());
     }
   }
+
+
 
   ByteData convertFileToByteData(File fileToRead){
     final file = fileToRead;
@@ -97,10 +98,89 @@ class _EncryptingScreenState extends State<EncryptingScreen> {
     return ByteData.view(bytes.buffer);
   }
 
-  Uint8List convertByteDataToString(ByteData byteData){
+  //The readable list
+  Uint8List convertByteDataToUint8List(ByteData byteData){
       ByteBuffer buffer = byteData.buffer;
       var list = buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes);
       return list;
+  }
+
+  //Given the encrypted file and the image, put file bytes into LSB of image
+  Uint8List putFileBytesIntoImgBytes(File file){
+    final bitsInByte = 8;
+    final byteData = convertFileToByteData(file);
+    final fileBytesList = convertByteDataToUint8List(byteData);
+    print("File bytes:");
+    print(fileBytesList);
+    final imgRGBBytes = decodeImageData();
+    var newImgRGBBytes = imgRGBBytes;
+
+    //Keyword that will be included after message so program knows where to stop decrypting
+    final keyword = "STOP";
+    //Keyword in bytes
+    List<int> keywordBytes = utf8.encode(keyword);
+  /*  print("keyword bytes");
+    print(keywordBytes);*/
+
+    int index = 0;
+
+    //Make dangerous assumption that there are more RGB bytes than bytes in file
+
+    //Iterate over all bytes in file
+    for(int i = 0; i <fileBytesList.length; i++){
+      print("Length of byte list:" + fileBytesList.length.toString()); //Need to incorporate this number at beginning of file to let program to run for how long
+      //Iterate over bits in byte, 8
+      var fileByte = convertIntToBits(fileBytesList[i]);
+      for(int j = 0; j < bitsInByte; j++){
+        newImgRGBBytes[index] = replaceLSBWithBit(imgRGBBytes[index], fileByte[j]);
+        index++;
+      }
+    }
+
+    //Add keyword message to end so decryptor knows when to stop
+    for(int i = 0; i<keywordBytes.length; i++){
+      var keywordByte = convertIntToBits(keywordBytes[i]);
+      for(int j = 0; j < bitsInByte; j++){
+        newImgRGBBytes[index] = replaceLSBWithBit(imgRGBBytes[index], keywordByte[j]);
+        index++;
+      }
+    }
+    print("new img:");
+    print(newImgRGBBytes);
+
+    return newImgRGBBytes;
+  }
+
+  void saveBytesAsFile(Uint8List byteList) async{
+    final path = await _localPath;
+    final file = File('$path/tempphoto.jpg').writeAsBytes(byteList);
+  }
+
+  void saveImage() async{
+    final path = await _localPath;
+  }
+
+  //Used to replace a byte's LSB with a new bit
+  int replaceLSBWithBit(int originalByte, String bit){
+    final byteString = convertIntToBits(originalByte);
+    final newByteString = byteString.substring(0, byteString.length - 1) + bit;
+    return convertBitsToInt(newByteString);
+  }
+
+
+  //Return a bit string of size 8
+  String convertIntToBits(int n){
+    var bits = n.toRadixString(2);
+    while(bits.length != 8){
+      bits = "0" + bits;
+    }
+    return bits;
+  }
+
+  //Convert bit string to an int
+  int convertBitsToInt(String bits){
+    final _pattern = RegExp(r'(?:0x)?(\d+)');
+    return int.parse(_pattern.firstMatch(bits)!.group(1)!, radix: 2);
   }
 
 /*  Color getColorAtPixel(Image image){
@@ -116,28 +196,36 @@ class _EncryptingScreenState extends State<EncryptingScreen> {
     return path;
   }
 
-  Future<List<List<List<int>>>> decodeImageData() async {
-    final Uint8List inputImg = (await rootBundle.load("images/dummy.jpeg")).buffer.asUint8List();
-    final Uint8List test;
-    final decoder = im.JpegDecoder();
-    final decodedImg = decoder.decodeImage(inputImg);
-    final decodedBytes = decodedImg.getBytes(format: im.Format.rgb);
-    decodedImageBytes = decodedBytes;
+    Uint8List decodeImageData() {
+    final im.Image? image = im.decodeImage(theImage.readAsBytesSync());
+    final imgBytes = image!.getBytes(format: im.Format.rgb);
+    print("Img bytes:");
+    print(imgBytes);
 
-    List<List<List<int>>> imgArr = [];
-    for(int y = 0; y < decodedImg.height; y++){
+    //Prints rgb values in [r,g,b] format
+    /*List<List<List<int>>> imgArr = [];
+    for(int y = 0; y < image.height; y++){
       imgArr.add([]);
-      for(int x = 0; x < decodedImg.width; x++){
-        int r = decodedBytes[y * decodedImg.width * 3 + x * 3];
-        int g = decodedBytes[y * decodedImg.width * 3 + x * 3 + 1];
-        int b = decodedBytes[y * decodedImg.width * 3 + x * 3 + 2];
+      for(int x = 0; x < image.width; x++){
+        int r = imgBytes[y * image.width * 3 + x * 3];
+        int g = imgBytes[y * image.width * 3 + x * 3 + 1];
+        int b = imgBytes[y * image.width * 3 + x * 3 + 2];
         imgArr[y].add([r,g,b]);
       }
-    }
-    print("rgb list");
-    print(imgArr);
-    return imgArr;
+    }*/
+    return imgBytes;
   }
+
+  /*Future<Uint8List> addEncryptedMsgToImg() async{
+    final path = await _localPath;
+    File encryptedFile = File('$path/encryption.txt.aes');
+
+    final Uint8List imgBytes = decodeImageData();
+    final ByteData byteData = convertFileToByteData(encryptedFile);
+    print("byte data using new method");
+    print(byteData);
+    return imgBytes;
+  }*/
 
 
 
@@ -233,7 +321,7 @@ class _EncryptingScreenState extends State<EncryptingScreen> {
                 ),
                 FloatingActionButton(
                     onPressed: () {
-                      encryptText(context, Text(HiddenMessageController.text).data, Text(PrivateKeyController.text).data);
+                      encryptText(context,Text(PrivateKeyController.text).data, Text(HiddenMessageController.text).data);
                     }
                 )
               ],
